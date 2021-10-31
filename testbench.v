983195f293382cc913097d2454bd4c62e4c66c32
Satwik Aman <72964606+satwikaman@users.noreply.github.com>
2021-10-31 09:12:24 +0530

    Add files via upload

--- testbench.v
+++ testbench.v
@@ -1,5 +1,5 @@
 /* Assignment : 2
-   Problem No : 1
+   Problem No : 3 (testbench)
    Semester : Autumn 2020
    Group No : G10
    Members : Ayudh Saxena & Anmol Harsh */
@@ -9,7 +9,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 11.09.2020 17:51:57
+// Create Date: 14.09.2020 15:57:49
 // Design Name: 
 // Module Name: testbench
 // Project Name: 
@@ -27,20 +27,27 @@
 
 
 module testbench;
-
-reg  [7:0] a,b;
+reg clk,write,clr;
+reg [7:0] a,b;
 wire [7:0] sum;
-reg cin;
 wire cout;
+bit_serial_adder m1(.clk(clk),.a(a),.write(write),.clr(clr),.sum(sum),.b(b),.cout(cout));
 
-ripple_carry_adder m1(.a(a),.b(b),.sum(sum),.cin(cin),.cout(cout)); // initialization of the ripple carry adder module 
 initial
 begin
-    $monitor("time = %0d, a = %b,b = %b, cin = %b,sum = %b,cout = %b",$time,a,b,cin,sum,cout);
-    #10 a = 255; b = 1; cin = 0;
-    #10 a = 244; b = 11; cin = 0;
-    #10 a = 127; b = 1; cin = 0;
-    #10 a = 6; b = 5; cin = 0;
-    #10 $finish;
-end  
+    clk = 0;
+    $monitor("time = %0d,write = %b,clr = %b,stop = %b,a = %b, b = %b,srega = %b, sregb = %b,sum = %b,cout = %b,count = %0d,sumbit = %b",$time,write,clr,m1.stop,a,b,m1.m1.wo,m1.m2.wo,sum,cout,m1.count,m1.sumbit);
+    a = 123; b = 145; clr = 1;
+    #30 write = 1; clr = 0;
+    #20 write = 0;
+    #200 clr = 1; 
+    #20 a = 16; b = 48; clr = 0;
+    #20 write = 1; // shift registers aren't loaded with a and b unless write = 1
+    #60 write = 0; // shifting doesn't begin unless write = 0
+    
+end
+
+always 
+    #10 clk = !clk;
+
 endmodule
